986f316f31809b56a2ef8945158615e38072e7f4
ben <ben@yahoo.com>
2022-09-14 12:27:57 +0200

    paragraph

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
 });
 
 Route::get('/hi', function () {
-    return 'hi Marjan';
+    return 'hi Marjan
+    
+| Here () is where you can register web routes for your application. These 33
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now creat
+    ';
 });
 
