4c70630696a3f1c6fe1181533089683a953222de
ben <ben@yahoo.com>
2022-09-19 21:25:27 +0200

    start of layout

--- app/Models/listing.php
+++ app/Models/listing.php
@@ -1,33 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'title' => 'laravel',
-                'description' => 'Here is where you can register web routes for your application. These 33
-                routes are loaded by the RouteServiceProvider within a group which
-                contains the "web" middleware group. Now create something great!'
-            ],
-            [
-                'id' => 2,
-                'title' => 'php',
-                'description' => 'Here is where you can register web routes for your application. These 33
-                routes are loaded by the RouteServiceProvider within a group which
-                contains the "web" middleware group. Now create something great!'
-            ]
-            ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if($listing['id'] == $id ){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          \App\Models\User::factory(10)->create();
+
+         Listing::factory(6)->create();
+
+        //  Listing::create([
+        //     'title' => 'Laravel junior Developer',
+        //     'tags' => 'laravel, javaScript, php',
+        //     'company' => 'Laravel CO',
+        //     'location' => 'Milan',
+        //     'email' => 'Laravelco@gmail.com',
+        //     'website' => 'Laravelco.com',
+        //     'description' => 'Laravel junior Developer. Laravel junior Developer. Laravel junior Developer. Laravel junior Developer',
+        //  ]);
+
+        //  Listing::create([
+        //     'title' => 'Backend Developer',
+        //     'tags' => 'Java, javaScript, php',
+        //     'company' => 'Master CO',
+        //     'location' => 'Rome',
+        //     'email' => 'master@gmail.com',
+        //     'website' => 'master.com',
+        //     'description' => 'Laravel junior Developer. Laravel junior Developer. Laravel junior Developer. Laravel junior Developer',
+        //  ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
