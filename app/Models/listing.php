da8616a2f42d4e7030a4e2d4c5c697c4621d1037
ben <ben@yahoo.com>
2022-09-19 17:44:00 +0200

    start of single listing(00:39:00)

---
+++ app/Models/listing.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+
+class Listing{
+    public static function all(){
+        return [
+            [
+                'id' => 1,
+                'title' => 'laravel',
+                'description' => 'Here is where you can register web routes for your application. These 33
+                routes are loaded by the RouteServiceProvider within a group which
+                contains the "web" middleware group. Now create something great!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'php',
+                'description' => 'Here is where you can register web routes for your application. These 33
+                routes are loaded by the RouteServiceProvider within a group which
+                contains the "web" middleware group. Now create something great!'
+            ]
+            ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Last Listings',
+        'listings' => Listing::all()
+    ]);
 });
 
 
